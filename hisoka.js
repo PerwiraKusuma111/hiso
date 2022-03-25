73494bad6d254ffea6e9c74e90da51d1eb460cf5
Lawliet <94941593+PerwiraKusuma111@users.noreply.github.com>
2022-03-25 17:36:09 +0700

    Add files via upload

--- hisoka.js
+++ hisoka.js
@@ -29,6 +29,7 @@ const { exec, spawn, execSync } = require("child_process")
 const axios = require('axios')
 const path = require('path')
 const os = require('os')
+const fetch = require('node-fetch')
 const moment = require('moment-timezone')
 const { JSDOM } = require('jsdom')
 const speed = require('performance-now')
@@ -1663,7 +1664,7 @@ conn.sendMessage(m.chat, listMessage)
             }
             break*/
 case 'pinterest': 
-			if(!text) return reply(`Masukkan query\n*Contoh :* ${prefix+command} Naruto`)
+			if(!q) return m.reply(`Masukkan query\n*Contoh :* ${prefix+command} Naruto`)
             async function pinterestSearch(query) {
                     return new Promise((resolve, reject) => {
                         fetch(`https://www.pinterest.com/resource/BaseSearchResource/get/?source_url=%2Fsearch%2Fpins%2F%3Fq%3D${query}&data=%7B%22options%22%3A%7B%22isPrefetch%22%3Afalse%2C%22query%22%3A%22${query}%22%2C%22scope%22%3A%22pins%22%2C%22no_fetch_context_on_resource%22%3Afalse%7D%2C%22context%22%3A%7B%7D%7D&_=1619980301559`, {
