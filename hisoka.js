6c28173e7cf8152b9ca97460f9393ab255c221e0
Lawliet <94941593+PerwiraKusuma111@users.noreply.github.com>
2022-03-30 20:29:36 +0700

    Add files via upload

--- hisoka.js
+++ hisoka.js
@@ -3173,8 +3173,8 @@ case 'online': {
 									break*/
 									case 'translate':
                                     case 'tr':
-                                    if (!text) throw `Contoh : ${prefix + command} en/Aku suka kamu`
-                                        try { {
+                                    if (text.includes("/")) {/*throw `Contoh : ${prefix + command} en/Aku suka kamu`*/
+                                        try {
                                     	let texti = args.join(" ")
                                         let text2 = texti.split("/")[0]
                                         let text1 = texti.split("/")[1]
@@ -3182,9 +3182,152 @@ case 'online': {
 let tr = require("translate-google-api")
 let _tr = await tr(`${text1}`, {to: text2})
 m.reply(_tr[0])
-}
 } catch(e) {
 m.reply(String(e))
+}
+} else if(!text.includes("/")) {/*throw `Contoh : ${prefix + command} en/Aku suka kamu`*/
+                                        try {
+                                    	let texti = args.join(" ")
+                                        let text2 = texti.split("/")[0]
+                                        let text1 = texti.split("/")[1]
+                                     /*   let totyp = m.quoted.text*/
+let tr = require("translate-google-api")
+let _tr = await tr(`${texti}`, {to: ''})
+m.reply(_tr[0])
+} catch(e) {
+m.reply(String(e))
+}
+} else if(quoted) {
+	if (/image/.test(mime)) return
+     if (/audio/.test(mime)) return
+      if (/video/.test(mime)) return
+	   if (/webp/.test(mime)) return
+	try {
+                                    	let texti = args.join(" ")
+                                        let text2 = texti[0]
+                                        let text1 = m.quoted.text
+                                     /*   let totyp = m.quoted.text*/
+let tr = require("translate-google-api")
+let _tr = await tr(`${text1}`, {to: text2})
+m.reply(_tr[0])
+} catch(e) {
+m.reply(String(e))
+}
+} else {
+	anu = `*Contoh :* \n${prefix+command} en/Thanks\nAtau\n${prefix+command} en(reply pesan)`
+let bttnz = [{buttonId: 'kodebahasa', buttonText: {displayText: 'Kode bahasa'}, type:1}]
+await conn.sendButtonText(m.chat, bttnz, anu, `Perwira Bot WhatsApp`, m)
+	}
+break
+case 'kodebahasa':{
+let leang = `*Kode Bahasa*
+
+Afrikaans = af
+Albanian = sq
+Amharic = am
+Arabic = ar
+Armenian = hy
+Azerbaijani = az
+Basque = eu
+Belarusian = be
+Bengali = bn
+Bosnian = bs
+Bulgarian = bg
+Catalan = ca
+Cebuano = ceb
+Chichewa = ny
+Chinese Simplified = zh-cn
+Chinese Traditional = zh-tw
+Corsican = co
+Croatian = hr
+Czech = cs
+Danish = da
+Dutch = nl
+English = en
+Esperanto = eo
+Estonian = et
+Filipino = tl
+Finnish = fi
+French = fr
+Frisian = fy
+Galician = gl
+Georgian = ka
+German = de
+Greek = el
+Gujarati = gu
+Haitian Creole = ht
+Hausa = ha
+Hawaiian = haw
+Hebrew = iw
+Hindi = hi
+Hmong = hmn
+Hungarian = hu
+Icelandic = is
+Igbo = ig
+Indonesian = id
+Irish = ga
+Italian = it
+Japanese = ja
+Javanese = jw
+Kannada = kn
+Kazakh = kk
+Khmer = km
+Korean = ko
+Kurdish (Kurmanji) = ku
+Kyrgyz = ky
+Lao = lo
+Latin = la
+Latvian = lv
+Lithuanian = lt
+Luxembourgish = lb
+Macedonian = mk
+Malagasy = mg
+Malay = ms
+Malayalam = ml
+Maltese = mt
+Maori = mi
+Marathi = mr
+Mongolian = mn
+Myanmar (Burmese) = my
+Nepali = ne
+Norwegian = no
+Pashto = ps
+Persian = fa
+Polish = pl
+Portuguese = pt
+Punjabi = ma
+Romanian = ro
+Russian = ru
+Samoan = sm
+Scots Gaelic = gd
+Serbian = sr
+Sesotho = st
+Shona = sn
+Sindhi = sd
+Sinhala = si
+Slovak = sk
+Slovenian = sl
+Somali = so
+Spanish = es
+Sundanese = su
+Swahili = sw
+Swedish = sv
+Tajik = tg
+Tamil = ta
+Telugu = te
+Thai = th
+Turkish = tr
+Ukrainian = uk
+Urdu = ur
+Uyghur = ug
+Uzbek = uz
+Vietnamese = vi
+Welsh = cy
+Xhosa = xh
+Yiddish = yi
+Yoruba = yo
+Zulu = zu`
+m.reply(leang)
 }
 break
             case 'ping': case 'botstatus': case 'statusbot': {
