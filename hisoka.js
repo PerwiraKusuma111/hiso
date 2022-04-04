548b26c4854bec3fa9198122daf14d1b1b6d269e
Lawliet <94941593+PerwiraKusuma111@users.noreply.github.com>
2022-04-04 19:49:25 +0700

    Add files via upload

--- hisoka.js
+++ hisoka.js
@@ -2043,7 +2043,7 @@ async function wikipedia(querry) {
 const link = await axios.get(`https://id.wikipedia.org/wiki/${querry}`)
 const $ = cheerio.load(link.data)
 let judul = $('#firstHeading').text().trim()
-let thumb = $("meta[property=\"og:image\"]").attr('content') || `//i.ibb.co/nzqPBpC/http-error-404-not-found.png`
+let thumb = $("meta[property=\"og:image\"]").attr('content') || `https://i.ibb.co/nzqPBpC/http-error-404-not-found.png`
 /*let thumb = $('#mw-content-text').find('div.mw-parser-output > div:nth-child(1) > table > tbody > tr:nth-child(2) > td > a > img').attr('src') || `//i.ibb.co/nzqPBpC/http-error-404-not-found.png`*/
 let isi = []
 $('#mw-content-text > div.mw-parser-output').each(function (rayy, Ra) {
@@ -2055,7 +2055,7 @@ for (let i of isi) {
 status: link.status,
 result: {
   judul: judul,
-  thumb: 'https:' + thumb,
+  thumb: thumb
   isi: i
 }
   }
