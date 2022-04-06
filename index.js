26b47de328328a616ab82a8c957505ef9a895591
Lawliet <94941593+PerwiraKusuma111@users.noreply.github.com>
2022-04-07 05:20:35 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -74,17 +74,17 @@ const PhoneNumber = require('awesome-phonenumber')
 const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('./lib/exif')
 const { smsg, isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, await, sleep } = require('./lib/myfunc')
 
-/*var low
+var low
 try {
 low = require('lowdb')
 } catch (e) {
 low = require('./lib/lowdb')
 }
 const { Low, JSONFile } = low
-*/
+
 global.api = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name] } : {}) })) : '')
 const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
-/*global.db = new Low(new JSONFile(`src/database.json`))*/
+global.db = new Low(new JSONFile(`src/database.json`))
 global.db.data = {
     users: {},
     chats: {},
