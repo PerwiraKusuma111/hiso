8f3c6eaae5a9db774af3ef335614237ecf9dbb68
Lawliet <94941593+PerwiraKusuma111@users.noreply.github.com>
2022-04-10 19:53:32 +0700

    Add files via upload

--- hisoka.js
+++ hisoka.js
@@ -2139,8 +2139,8 @@ async function wikipedia(querry) {
 const link = await axios.get(`https://id.wikipedia.org/wiki/${querry}`)
 const $ = cheerio.load(link.data)
 let judul = $('#firstHeading').text().trim()
-let thumb = $("meta[property=\"og:image\"]").attr('content') || `https://i.ibb.co/nzqPBpC/http-error-404-not-found.png`
-let thumbw = await getBuffer(thumb)
+let thumbwik = $("meta[property=\"og:image\"]").attr('content') || `https://i.ibb.co/nzqPBpC/http-error-404-not-found.png`
+let thumbw = await getBuffer(thumbwik)
 /*let thumb = $('#mw-content-text').find('div.mw-parser-output > div:nth-child(1) > table > tbody > tr:nth-child(2) > td > a > img').attr('src') || `//i.ibb.co/nzqPBpC/http-error-404-not-found.png`*/
 let isi = []
 $('#mw-content-text > div.mw-parser-output').each(function (rayy, Ra) {
@@ -2167,14 +2167,18 @@ return notFond
   }
 }
 
+try {
 wikipedia(text).then(res => {
 	kunu = `*Judul :* ${res.result.judul}
 	
 	*Hasil penelusuran*
 	${res.result.isi}
 	`
-conn.sendMessage(m.chat, {image: {url: res.result.thumb }, caption: kunu, contextInfo: {externalAdReply: {title: `${title}`, body: "Perwira Bot WhatsApp", mediaUrl: `https://id.wikipedia.org/wiki/${text}`, sourceUrl: `https://id.wikipedia.org/wiki/${text}`, mediaType: 1, thumbnail: thumbw}}})
+conn.sendMessage(m.chat, {image: {url: thumbwik }, caption: kunu, contextInfo: {externalAdReply: {title: `${res.result.title}`, body: "Perwira Bot WhatsApp", mediaUrl: `https://id.wikipedia.org/wiki/${text}`, sourceUrl: `https://id.wikipedia.org/wiki/${text}`, mediaType: 1, thumbnail: thumbw}}})
 })
+} catch(err) {
+	m.reply(util.format(err))
+	}
 }break
 case 'pinterest': 
 			if(!q) return m.reply(`Masukkan query\n*Contoh :* ${prefix+command} Naruto`)
