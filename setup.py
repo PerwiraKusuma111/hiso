445325184d9c1759d04b1f3591495be2b399c21c
Lawliet <94941593+PerwiraKusuma111@users.noreply.github.com>
2022-10-10 21:52:11 +0700

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-import os
-import re
-from setuptools import setup, find_packages
-INSTALL_REQUIRES = ['requests',
-                    'tqdm'
-                    ]
-setup(
-install_requires=INSTALL_REQUIRES
-)
